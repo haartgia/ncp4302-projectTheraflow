6b26bd68ae3b01473b6f24fcc06aaaadc44f7e5d
Joezer29 <cornita.joezer@ue.edu.ph>
2026-03-14 23:01:01 +0800

    create dashboard

---
+++ script.js
@@ -0,0 +1,11 @@
+const menuItems = document.querySelectorAll(".menu li");
+
+menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+
+        menuItems.forEach(i => i.classList.remove("active"));
+
+        item.classList.add("active");
+
+    });
+});
